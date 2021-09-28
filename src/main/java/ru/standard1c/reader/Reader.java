68a37c87f668a811df383253afce0b34d80a5683
Maxim Tereshchenko <tereshchenko.xini@gmail.com>
2021-09-28 14:47:03 +0300

    separate configuration from Reader interface

--- src/main/java/ru/standard1c/reader/Reader.java
+++ src/main/java/ru/standard1c/reader/Reader.java
@@ -3,32 +3,17 @@ package ru.standard1c.reader;
 import ru.standard1c.reader.source.Attribute;
 import ru.standard1c.reader.source.AttributeSource;
 
-import java.util.function.BiConsumer;
-import java.util.function.Function;
-
 /**
  * Абстракция инструмента для чтения последовательности
  * {@link Attribute} из {@link AttributeSource}.
  *
  * @author Maxim Tereshchenko
  */
-public interface Reader<T, R> {
+public interface Reader<T> {
 
     String startOfSectionAttributeKey();
 
     String endOfSectionAttributeKey();
 
-    Reader<T, R> onAttribute(String key, BiConsumer<T, String> valueConsumer);
-
-    <E> Reader<T, R> onAttribute(String key, Function<String, E> mapper, BiConsumer<T, E> valueConsumer);
-
-    <S, U> Reader<T, R> onSection(Reader<S, U> reader, BiConsumer<T, U> sectionConsumer);
-
-    <U> Reader<T, U> onEndOfSection(Function<T, U> finisher);
-
-    Reader<T, R> onAnyOtherAttribute(BiConsumer<T, Attribute> attributeConsumer);
-
-    Reader<T, R> failOnUnknownAttribute();
-
-    R read(AttributeSource attributeSource);
+    T read(AttributeSource attributeSource);
 }
